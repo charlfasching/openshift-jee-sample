526bee115c43725740d994a1825f3dfcccf66942
Charl Fasching <Charl@mediswitch.co.za>
2017-03-24 12:39:36 +0200

    Add Rest Test

---
+++ src/main/java/za/co/charl/rest/RestActivator.java
@@ -0,0 +1,12 @@
+package za.co.charl.rest;
+
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestActivator extends Application {
+ 
+   
+}
---
+++ src/main/java/za/co/charl/rest/TestService.java
@@ -0,0 +1,53 @@
+package za.co.charl.rest;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/test")
+@Produces(MediaType.APPLICATION_JSON)
+public class TestService {
+
+    
+    
+    @GET
+    @Path("/")
+    public Response hello() {
+        return Response.ok("what?").build();
+    }
+
+    @GET
+    @Path("/list")
+    public Response list() {
+        List<String> atrs = Arrays.asList("Attribute1","Attribute2");
+        return Response.ok(atrs).build();
+    }
+    
+    @POST
+    @Path("/save")
+    public Response save() {
+        return Response.ok("Saved").build();
+    }
+
+    @GET
+    @Path("/test")
+    public Response test() {
+        return Response.ok("Tested").build();
+    }
+    
+    @GET
+    @Path("/other")
+    public Response other() {
+        return Response.ok("Other Worldly").build();
+    }
+
+  
+}
